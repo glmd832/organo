dcbb042de885567ea4dfbbfe7e8618f6ea78b76b
glmd832 <glmd832@gmail.com>
2024-03-13 13:38:36 -0300

    First commit

---
+++ src/componentes/Banner/index.js
@@ -0,0 +1,5 @@
+import { Banner } from "./Banner";
+
+export default Banner
+
+//essa classe serve para eu poder escolher qual componete eu posso escolher exportar, para os componentes não ficarem todos globais
